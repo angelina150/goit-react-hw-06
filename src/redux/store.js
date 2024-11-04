37e7faeb67d91c6e2907d109d0fa239801e580cf
Angelina Chigiryova <chigiryova31@gmail.com>
2024-11-04 18:58:00 +0200

    changed store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -15,7 +15,7 @@ import storage from "redux-persist/lib/storage";
 const persistConfig = {
   key: "contactsKey",
   storage,
-  whitelist: ["items"],
+  whitelist: ["contacts"],
 };
 
 export const store = configureStore({
